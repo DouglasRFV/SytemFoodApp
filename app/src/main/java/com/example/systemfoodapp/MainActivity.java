c5895c653a38d3358740f9c34a5aed6c094d2062
Leandro Luiz Cipriano <lean151utilities@gmail.com>
2021-05-08 10:59:46 -0300

    Revert "Telas Login e Cadastro"
    This reverts commit fa1128c0

--- app/src/main/java/com/example/systemfoodapp/MainActivity.java
+++ app/src/main/java/com/example/systemfoodapp/MainActivity.java
@@ -2,28 +2,13 @@ package com.example.systemfoodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tela_login);
-        btn_login= findViewById(R.id.btn_login);
-
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Tela_Cadastro.class);
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_main);
     }
 }
