ff20fa09db0b98eb8fba8b846a5d41606ee0e8f4
Douglas Viana <douglas.rfvianna@gmail.com>
2021-05-26 23:58:58 -0300

    logica login e cadastro

--- app/src/main/java/com/example/systemfoodapp/LoginActivity.java
+++ app/src/main/java/com/example/systemfoodapp/LoginActivity.java
@@ -3,15 +3,34 @@ package com.example.systemfoodapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    AppCompatEditText edit_usuario, edit_senha;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edit_usuario = (AppCompatEditText) findViewById(R.id.edit_usuario);
+        edit_senha = (AppCompatEditText) findViewById(R.id.edit_senha);
+        
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Boa amigão!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
