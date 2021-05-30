58b3aba33b31d1ece3e82ee56cdbe04e886b74ad
Douglas Viana <douglas.rfvianna@gmail.com>
2021-05-30 18:11:20 -0300

    login com webservice

--- app/src/main/java/com/example/systemfoodapp/MainActivity.java
+++ app/src/main/java/com/example/systemfoodapp/MainActivity.java
@@ -9,21 +9,11 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_login;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        btn_login= findViewById(R.id.btn_login);
+        setContentView(R.layout.activity_main);
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
