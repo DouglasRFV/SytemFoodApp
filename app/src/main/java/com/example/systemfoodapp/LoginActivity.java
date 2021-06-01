508e5532849d2076f95094e644a237d4f2a20cd0
Douglas Viana <douglas.rfvianna@gmail.com>
2021-06-01 01:55:36 -0300

    Criptografia Login done

--- app/src/main/java/com/example/systemfoodapp/LoginActivity.java
+++ app/src/main/java/com/example/systemfoodapp/LoginActivity.java
@@ -19,6 +19,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.systemfoodapp.modelo.Criptografia;
 
 import org.json.JSONObject;
 
@@ -27,7 +28,9 @@ import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    String urlWebService = "http://192.168.0.23/systemfood/getUsuarios.php";
+    String urlWebService = "http://192.168.0.3/systemfood/getUsuarios.php";
+    String loginCriptografado;
+    String senhaCriptografada;
     StringRequest stringRequest;
     RequestQueue requestQueue;
 
@@ -63,51 +66,72 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (validado) {
                     Toast.makeText(LoginActivity.this, "Validando seus dados...aguarde.", Toast.LENGTH_SHORT).show();
-                    validarLogin();
-                }
+                    criptografar();
+
+            }
 
             }
         });
     }
 
+    private void criptografar() {
+        try {
+            Criptografia criptografia = new Criptografia();
+            String login = criptografia.encrypt(edit_login.getText().toString());
+            String senha = criptografia.encrypt(edit_senha.getText().toString());
+            Log.v("Criptografia Login", login);
+            Log.v("Criptografia Senha", senha);
+            loginCriptografado = login;
+            senhaCriptografada = senha;
+            validarLogin();
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+    }
+
     private void validarLogin() {
 
         stringRequest = new StringRequest(Request.Method.POST, urlWebService,
-                response -> {
-                    Log.v("LogLogin", response);
-                    try {
-                        JSONObject jsonObject = new JSONObject(response);
+               new Response.Listener<String>() {
+                   @Override
+                   public void onResponse(String response) {
+                       try {
+                           JSONObject jsonObject = new JSONObject(response);
 
-                        boolean isErro = jsonObject.getBoolean("erro");
+                           boolean isErro = jsonObject.getBoolean("erro");
 
-                        if(isErro) {
-                            Toast.makeText(getApplicationContext(), "Dados Inválidos", Toast.LENGTH_LONG).show();
-                        } else {
-                            int tipoUsuario = jsonObject.getInt("tipoUsuario");
+                           if(isErro) {
+                               Toast.makeText(getApplicationContext(), "Dados Inválidos", Toast.LENGTH_LONG).show();
+                           } else {
+                               int tipoUsuario = jsonObject.getInt("tipoUsuario");
 
-                            if(tipoUsuario == 1) {
-                                Intent tela = new Intent(this, MainActivity.class);
-                                startActivity(tela);
-                                finish();
-                            } else if(tipoUsuario == 2){
-                                Intent tela = new Intent(this, ClienteDescontoActivity.class);
-                                startActivity(tela);
-                                finish();
-                            }
-                        }
-                    } catch (Exception e) {
-                        Log.v("LogLogin", e.getMessage());
-                    }
-                },
-                error -> {
-                    String err = (error.getMessage()==null)?"Sem mensagem de erro":error.getMessage();
-                    Log.e("LogLogin", err);
-                }) {
+                               if(tipoUsuario == 1) {
+                                   Intent tela = new Intent(LoginActivity.this, MainActivity.class);
+                                   startActivity(tela);
+                                   finish();
+                               } else if(tipoUsuario == 2){
+                                   Intent tela = new Intent(LoginActivity.this, ClienteDescontoActivity.class);
+                                   startActivity(tela);
+                                   finish();
+                               }
+                           }
+                       } catch (Exception e) {
+                           Log.v("LogLoginErro", e.getMessage());
+                       }
+                   }
+               },
+               new Response.ErrorListener() {
+                   @Override
+                   public void onErrorResponse(VolleyError error) {
+                       String err = (error.getMessage()==null)?"Sem mensagem de erro":error.getMessage();
+                       Log.e("LogLogin", err);
+                   }
+               }) {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("login", edit_login.getText().toString());
-                params.put("senha", edit_senha.getText().toString());
+                params.put("login", loginCriptografado);
+                params.put("senha", senhaCriptografada);
                 return params;
             }
         };
